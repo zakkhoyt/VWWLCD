f4089906fa218f8d4e602caeda760dad80f30c82
Zakk Hoyt <vaporwarewolf@gmail.com>
2013-11-09 20:44:47 -0800

    cleaned up unused code

--- VWWLCD.cpp
+++ VWWLCD.cpp
@@ -30,7 +30,7 @@ VWWLCD::VWWLCD(){
   };
   */
 
-  this->draw_state = 0;
+
   u8g = new U8GLIB_ST7920_128X64(13, 11, 12, U8G_PIN_NONE);
   
   // flip screen, if required
@@ -54,69 +54,6 @@ VWWLCD::VWWLCD(){
   }
 }
 
-/*void VWWLCD::draw(){
-
-    // graphic commands to redraw the complete screen should be placed here  
-  u8g->setFont(u8g_font_unifont);
-  //u8g->setFont(u8g_font_osb21);
-  u8g->drawStr( 0, 22, "Hello World!");
-}
-*/
-
-
-void VWWLCD::drawString(String string){
-  char charBuf[string.length()];
-  string.toCharArray(charBuf, string.length());
-  u8g->drawStr(0, 22, charBuf);
-}
-
-void VWWLCD::drawStringFromDigit(unsigned int digit){
-  switch(digit){
-    case 0:
-    break;
-    case 1:{
-      u8g->drawStr(0, 22, "Subscription :D");
-      break;
-    }
-    case 2:{
-      u8g->drawStr(0, 22, "Subscription renewal :)");
-      break;
-    }
-    case 3:
-    u8g->drawStr(0, 22, "Cancellation... DOH! :(");
-      break;
-      default:
-      break;
-
-    }
-  }
-
-  void VWWLCD::drawCycle(){
-/*    // picture loop
-    u8g->firstPage();  
-    do {
-      draw();
-    } while( u8g->nextPage() );*/
-  // picture loop  
-
-  u8g->firstPage();  
-  do {
-    draw();
-  } while( u8g->nextPage() );
-  
-  // increase the state
-  draw_state++;
-  if ( draw_state >= 8*8 )
-    draw_state = 0;
-  
-  // rebuild the picture after some delay
-    delay(250);
-  }
-
-
-
-
-
 
 
 
@@ -127,125 +64,12 @@ void VWWLCD::drawStringFromDigit(unsigned int digit){
     u8g->setFontPosTop();
   }
 
-  void VWWLCD::u8g_box_frame(uint8_t a) {
-    u8g->drawStr( 0, 0, "VWW drawBox");
-    u8g->drawBox(5,10,20,10);
-    u8g->drawBox(10+a,15,30,7);
-    u8g->drawStr( 0, 30, "drawFrame");
-    u8g->drawFrame(5,10+30,20,10);
-    u8g->drawFrame(10+a,15+30,30,7);
-  }
-
-  void VWWLCD::u8g_disc_circle(uint8_t a) {
-    u8g->drawStr( 0, 0, "VWW drawDisc");
-    u8g->drawDisc(10,18,9);
-    u8g->drawDisc(24+a,16,7);
-    u8g->drawStr( 0, 30, "VWW drawCircle");
-    u8g->drawCircle(10,18+30,9);
-    u8g->drawCircle(24+a,16+30,7);
-  }
-
-  void VWWLCD::u8g_r_frame(uint8_t a) {
-    u8g->drawStr( 0, 0, "VWW drawRFrame/Box");
-    u8g->drawRFrame(5, 10,40,30, a+1);
-    u8g->drawRBox(50, 10,25,40, a+1);
-  }
-
-  void VWWLCD::u8g_string(uint8_t a) {
-    u8g->drawStr(30+a,31, " 0");
-    u8g->drawStr90(30,31+a, " 90");
-    u8g->drawStr180(30-a,31, " 180");
-    u8g->drawStr270(30,31-a, " 270");
-  }
-
-  void VWWLCD::u8g_line(uint8_t a) {
-    u8g->drawStr( 0, 0, "VWW drawLine");
-    u8g->drawLine(7+a, 10, 40, 55);
-    u8g->drawLine(7+a*2, 10, 60, 55);
-    u8g->drawLine(7+a*3, 10, 80, 55);
-    u8g->drawLine(7+a*4, 10, 100, 55);
-  }
-
-  void VWWLCD::u8g_ascii_1() {
-    char s[2] = " ";
-    uint8_t x, y;
-    u8g->drawStr( 0, 0, "VWW ASCII page 1");
-    for( y = 0; y < 6; y++ ) {
-      for( x = 0; x < 16; x++ ) {
-        s[0] = y*16 + x + 32;
-        u8g->drawStr(x*7, y*10+10, s);
-      }
-    }
-  }
-
-  void VWWLCD::u8g_ascii_2() {
-    char s[2] = " ";
-    uint8_t x, y;
-    u8g->drawStr( 0, 0, "VWW ASCII page 2");
-    for( y = 0; y < 6; y++ ) {
-      for( x = 0; x < 16; x++ ) {
-        s[0] = y*16 + x + 160;
-        u8g->drawStr(x*7, y*10+10, s);
-      }
-    }
-  }
-
-  void VWWLCD::u8g_extra_page(uint8_t a)
-  {
-    if ( u8g->getMode() == U8G_MODE_HICOLOR || u8g->getMode() == U8G_MODE_R3G3B2) {
-    /* draw background (area is 128x128) */
-      u8g_uint_t r, g, b;
-      b = a << 5;
-      for( g = 0; g < 64; g++ )
-      {
-        for( r = 0; r < 64; r++ )
-        {
-         u8g->setRGB(r<<2, g<<2, b );
-         u8g->drawPixel(g, r);
-       }
-     }
-     u8g->setRGB(255,255,255);
-     u8g->drawStr( 66, 0, "VWW Color Page");
-   }
-   else if ( u8g->getMode() == U8G_MODE_GRAY2BIT )
-   {
-    u8g->drawStr( 66, 0, "VWW Gray Level");
-    u8g->setColorIndex(1);
-    u8g->drawBox(0, 4, 64, 32);    
-    u8g->drawBox(70, 20, 4, 12);
-    u8g->setColorIndex(2);
-    u8g->drawBox(0+1*a, 4+1*a, 64-2*a, 32-2*a);
-    u8g->drawBox(74, 20, 4, 12);
-    u8g->setColorIndex(3);
-    u8g->drawBox(0+2*a, 4+2*a, 64-4*a, 32-4*a);
-    u8g->drawBox(78, 20, 4, 12);
-
-  }
-  else
-  {
-    u8g->drawStr( 0, 12, "VWW setScale2x2");
-    u8g->setScale2x2();
-    u8g->drawStr( 0, 6+a, "VWW setScale2x2");
-    u8g->undoScale();
-  }
-}
-
-
-
-
+ 
 
 void VWWLCD::u8g_barGraphFromSales(char* title, uint8_t *subSales, uint8_t size) {
-    //u8g->drawStr( 0, 0, "Subscription");
-    //u8g->drawBox(5,10,20,10);
-    //u8g->drawBox(10+a,15,30,7);
 
-
-
-  //const uint8_t dayWidth = 4;
   const uint8_t dayWidth = graphWidth / (float)size;
   const uint8_t dayXSpacing = 1;
-    //const uint8_t saleHeight = 2;
-
 
   uint8_t max = 0;
   uint8_t maxIndex = 0;
@@ -253,9 +77,6 @@ void VWWLCD::u8g_barGraphFromSales(char* title, uint8_t *subSales, uint8_t size)
   uint8_t minIndex = 0;
 
   for(uint8_t saleIndex = 0; saleIndex < size; saleIndex++){
-      // Temp: Populate subSales
-    //subSales[saleIndex] = random(0, 20);
-
     if(subSales[saleIndex] > max){
       max = subSales[saleIndex];
       maxIndex = saleIndex;
@@ -270,28 +91,33 @@ void VWWLCD::u8g_barGraphFromSales(char* title, uint8_t *subSales, uint8_t size)
   const float saleHeight = graphHeight / (float)max;
 
   char str[128] = "";
-  //sprintf(str, "max=%d min=%d sh=%d\r\n", max, min, (uint8_t)saleHeight);
   sprintf(str, "%s: max=%d min=%d", title, max, min);
 
 
   u8g->drawStr( 0, 0, str);
-
     uint8_t graphYOffset = (screenHeight - graphHeight);
     for(uint8_t day = 0; day < 30; day++){
-
       uint8_t x = 0, y = 0, w = 0, h = 0;
       x = day * dayWidth;
       y = graphYOffset + graphHeight - (subSales[day] * saleHeight);
       w = dayWidth;
       h = graphYOffset + graphHeight - y;
-
       u8g->drawFrame(x, y, w, h);
-
     }
   }
 
 
 
+
+  void VWWLCD::drawCycle(){
+
+    u8g->firstPage();  
+    do {
+      draw();
+    } while( u8g->nextPage() );
+
+    delay(250);
+  }
 
 // 128x64
 void VWWLCD::draw(void) {
@@ -301,16 +127,11 @@ void VWWLCD::draw(void) {
   // uint8_t subSales[30] = {1, 2, 20 ,50, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   uint8_t subSales[30] = {1, 2, 20 , 10, 5, 6, 7, 8, 9, 10};
   u8g_barGraphFromSales("Sales", subSales, 10);
-  //
-  /*switch(draw_state >> 3) {
-    case 0: u8g_box_frame(draw_state&7); break;
-    case 1: u8g_disc_circle(draw_state&7); break;
-    case 2: u8g_test(draw_state&7);*/
-/*    case 2: u8g_r_frame(draw_state&7); break;
-    case 3: u8g_string(draw_state&7); break;
-    case 4: u8g_line(draw_state&7); break;*/
-    /*case 5: u8g_ascii_1(); break;
-    case 6: u8g_ascii_2(); break;
-    case 7: u8g_extra_page(draw_state&7); break;
-  }*/
+  
 }
+
+
+
+
+
+
--- VWWLCD.h
+++ VWWLCD.h
@@ -6,26 +6,15 @@
 class VWWLCD{
 private: 
 	U8GLIB_ST7920_128X64 *u8g;
-	uint8_t draw_state;
+
 
 	void u8g_prepare(void);
-	void u8g_box_frame(uint8_t a);
-	void u8g_disc_circle(uint8_t a);
-	void u8g_r_frame(uint8_t a);
-	void u8g_string(uint8_t a);
-	void u8g_line(uint8_t a);
-	void u8g_ascii_1();
-	void u8g_ascii_2();
-	void u8g_extra_page(uint8_t a);
 	void u8g_barGraphFromSales(char* title, uint8_t *subSales, uint8_t size);
 
 public:
 	VWWLCD();
 
-	void drawString(String string);
-	void drawStringFromDigit(unsigned int digit);
 	void drawCycle();
-
 	void draw(void); 
 
 };
